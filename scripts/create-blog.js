6f4c5db3e4e6051914e1ebb1ad4e5feab6d0c00f
code-tieumomo <code.tieumomo@gmail.com>
2022-11-19 16:22:39 +0700

    Update

--- scripts/create-blog.js
+++ scripts/create-blog.js
@@ -1,3 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    push,
+    set,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDeX1BVGvu6ys7teuPYT6qn4edv0vQ9lcQ",
+    authDomain: "hieu-s-lair.firebaseapp.com",
+    projectId: "hieu-s-lair",
+    storageBucket: "hieu-s-lair.appspot.com",
+    messagingSenderId: "70240476202",
+    appId: "1:70240476202:web:8e6e2dac47e0cba3e33753",
+    databaseURL: "https://hieu-s-lair-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
 document.addEventListener("DOMContentLoaded", async function () {
     if (localStorage.getItem("user")) {
         document.querySelector("#login").remove();
@@ -16,6 +43,25 @@ document.addEventListener("DOMContentLoaded", async function () {
         const title = document.querySelector("#title").value;
         const category = document.querySelector("#category").value;
         const summary = document.querySelector("#summary").value;
-        
+
+        editor
+            .save()
+            .then((outputData) => {
+                // Bắt buộc phải xử lý ở đây vì editor.save() trả về Promise (bất đồng bộ)
+                // Lưu dữ liệu vào database bằng hàm set
+                set(push(ref(database, "blogs/")), {
+                    title,
+                    category,
+                    summary,
+                    content: outputData,
+                    thumb: "https://picsum.photos/1920/1080",
+                    author: JSON.parse(localStorage.getItem("user")).email.split("@")[0],
+                }).then(function (isSaved) {
+                    alert("Lưu thành công");
+                });
+            })
+            .catch((error) => {
+                console.log("Saving failed: ", error);
+            });
     };
 });
